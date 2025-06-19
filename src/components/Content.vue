6564b5427d665035290e8cc64f8eaa0fc60dd96e
Guillaume Gorvel <ggorvel@gmail.com>
2025-06-19 18:51:35 +0200

    Ajout de nouveaux éléments graphiques avec des images SVG et mise à jour du composant Content pour intégrer des effets de survol.

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -14,8 +14,14 @@
             </p>
         </div>
         <div class="content_image">
-            <img class="hex-img" src="@/assets/img/hex-2.png" alt="green" />
-            <img class="hex-img offset" src="@/assets/img/hex-1.png" alt="green" />
+            <div class="hex-img-hover">
+                <img class="hex-img" src="@/assets/img/hex-2.png" alt="green" />
+                <img class="poly" src="@/assets/img/poly-1.svg" alt="polygon">
+            </div>
+            <div class="hex-img-hover">
+                <img class="hex-img offset" src="@/assets/img/hex-1.png" alt="green" />
+                <img class="poly-2" src="@/assets/img/poly-2.svg" alt="polygon">
+            </div>
 
         </div>
     </div>
@@ -26,6 +32,7 @@
 section {
    padding: 4.8rem 0;
 }
+
 .content {
     display: flex;
     align-items: flex-start;
@@ -39,18 +46,52 @@ section {
     line-height: 1.5;
     color: #737373;
 }
-.content-image{
+.content_image{
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   gap: 4rem;
   padding: 2rem;
+  width: 100%;
 }
-
-.hex-img {
+.hex-img-hover {
+  position: relative;
   width: 250px;
   height: auto;
 }
+
+.hex-img {
+  width: 100%;
+  display: block;
+  position: relative;
+  z-index: 2;
+}
+
+.poly {
+  width: 100%;
+  position: absolute;
+  top: 2px;
+  left: 0;
+  z-index: 1;
+  transition: transform 0.3s ease;
+}
+.poly-2{
+   width: 100%;
+  position: absolute;
+  top: 10px;
+  left: 80px;
+  z-index: 1;
+  transition: transform 0.3s ease;
+}
+
+
+.hex-img-hover:hover .poly{
+  transform: translate(12px, 12px);
+}
+.hex-img-hover:hover .poly-2 {
+    transform: translate(12px, 24px);
+}
+
 
 .offset {
   margin-left: 80px; 
